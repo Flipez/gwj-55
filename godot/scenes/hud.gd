1c8aa757295b6a8f9dc1cc02cbe3a6fbf94cd9de
Kjarrigan <github@kjarrigan.de>
2023-03-11 22:10:29 +0100

    Journal: Fix signal handling

--- godot/scenes/hud.gd
+++ godot/scenes/hud.gd
@@ -5,20 +5,21 @@ extends CanvasLayer
 @onready var journal = $Journal
 
 func _ready():
-	journal.connect("update_journal", _update_journal)
+	Globals.connect("update_journal", _update_journal)
 
 func _process(delta):
 	pass
 
 func _update_journal(_msg):
+	print_debug(hide_status.wait_time)
 	status_message.text = "Journal updated"
-	hide_status.start()
+	hide_status.start(2.0)
 	
 func _on_hide_status_timeout():
 	status_message.text = ""
 
 func _on_button_pressed():
-	journal.emit_signal("update_journal", "Hello World")
+	Globals.emit_signal("update_journal", "Hello World")
 	if journal.visible:
 		journal.hide()
 	else:
--- godot/scenes/journal.gd
+++ godot/scenes/journal.gd
@@ -1,15 +1,14 @@
 extends Control
 class_name Journal
 
-signal update_journal(content: String)
-var pages = ["03/10/2023 - Herzmut Games, Munich\n\nI arrived 10:03 at the crime scene - the lunch room in the HQ of the famous Game-Studio."]
+var pages = ["03/10/2023 - Herzmut Games, Munich\n\nI arrived 10:03 at the crime scene - the lunch room in the HQ of the famous Game-Studio.\n"]
 var current_page = 0
 
 @onready var left_page = $Page/Left/Content
 @onready var right_page = $Page/Right/Content
 
 func _ready():
-	connect("update_journal", _on_update_journal)
+	Globals.connect("update_journal", _on_update_journal)
 	turn_page(0)
 
 func turn_page(direction):
@@ -41,17 +40,24 @@ func _on_next_page_pressed():
 	turn_page(2)
 
 func _on_update_journal(content: String):
-	var page_data = pages[-1]
+	var page_data = pages.pop_back()
 	var char_count = page_data.count("\n") * 42 + len(page_data)
 	
-	if char_count < 500:
-		for word in content.split(" ", false):
-			page_data += " "
-			page_data += word
-			char_count += len(word) + 1
+	for word in content.split(" ", false):
+		page_data += word
+		page_data += " "
+		char_count += len(word) + 1
 
-			# No additional checking on page wrap for another overflow 
-			# since the text-chunks are small
-			if char_count >= 600:
-				page_data = ""
-				pages.push_back(page_data)
+		# No additional checking on page wrap for another overflow 
+		# since the text-chunks are small
+		if char_count >= 800:
+			pages.push_back(page_data)
+			page_data = ""
+	page_data += "\n"
+	char_count += 42
+	pages.push_back(page_data)
+	if char_count > 700:
+		pages.push_back("")
+
+	# Refresh
+	turn_page(0)
