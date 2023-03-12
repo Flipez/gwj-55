078c3070da4974f118dcb8011620a9048308de75
Flipez <code@brauser.io>
2023-03-12 17:20:25 +0100

    add more interaction examples

---
+++ godot/props/office/plant_1.gd
@@ -0,0 +1,4 @@
+extends StaticBody2D
+
+func interact():
+	Globals.emit_signal("initiate_chat", "This is a plant. Seems to have nothing special to it.")
---
+++ godot/props/office/soda.gd
@@ -0,0 +1,4 @@
+extends StaticBody2D
+
+func interact():
+	Globals.emit_signal("initiate_chat", "A vending machine full of Spezi. I should ask Robert which one is the best!")
