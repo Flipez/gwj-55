11db180965d19b68522ced57a6226b71441a64db
Kjarrigan <github@kjarrigan.de>
2023-03-14 20:50:05 +0100

    Add npc base-scene

--- godot/libs/interaction_handler.gd
+++ godot/libs/interaction_handler.gd
@@ -4,9 +4,11 @@ var objects = {
 	"AAB" = func(): Globals.chat("This is a plant"), # Generic plant
 }
 
-
 func interact(id : String):
 	if objects.has(id):
 		objects[id].call()
 	else:
 		Globals.chat("Lt. Oak says: This itsn't the time to use that.")
+
+func intro():
+	pass
